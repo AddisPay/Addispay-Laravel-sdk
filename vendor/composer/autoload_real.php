bd3aa5bdeafc916cff3f07b8d36ceb9b7569abfe
Ashenafi Alemu <ashenafialemu27@gmail.com>
2024-10-01 10:39:32 +0300

    AddisPay Payment Gateway SDK

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit327cf22abc84ef0bd815ecd1488ba05c
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit327cf22abc84ef0bd815ecd1488ba05c::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
